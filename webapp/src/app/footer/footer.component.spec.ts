74fb1706055ee80a5eae1ef16023d7c3749c147e
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2023-06-25 17:25:40 -0400

    nice unit tests for footer

--- webapp/src/app/footer/footer.component.spec.ts
+++ webapp/src/app/footer/footer.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FooterComponent } from './footer.component';
+import { By } from '@angular/platform-browser';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -15,7 +15,17 @@ describe('FooterComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('displays the text "Made by Jim"', () => {
+    const madeByJim = fixture.debugElement.query(By.css('p'))
+    expect(madeByJim.nativeNode.innerText.trim()).toBe('Made by Jim. ☕️');
   });
+
+  it('displays "Jim" as a link to his github page', () => {
+
+    const link = fixture.debugElement.query(By.css('a'))
+
+    expect(link.nativeNode.outerText).toEqual("Jim");
+    expect(link.nativeNode.href).toEqual("https://twitter.com/JimLynchCodes");
+  });
+
 });
